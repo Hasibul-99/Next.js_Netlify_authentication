3244a4ad2b13c5a1d0522f95318d1a27b53d3aa6
hasibul hasan <hasibul.hasan@sslwireless.com>
2021-08-20 01:44:43 +0600

    update condition

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,17 +13,17 @@ export default function Navbar() {
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
+        {
+            authReady ? 
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
           {
-            authReady ? <> {
-              user ? <li onClick={logout} className="btn">Logout</li> 
-              : <li onClick={login} className="btn">Login/Signup</li>
-            } </> : ''
+            user ? <li onClick={logout} className="btn">Logout</li> 
+            : <li onClick={login} className="btn">Login/Signup</li>
           }
           
-        </ul>
+        </ul> : ''}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
