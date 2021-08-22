f447e57e0890c7910aeb0bbc777a006df974401b
hasibul hasan <hasibul.hasan@sslwireless.com>
2021-08-22 11:25:02 +0600

    netlife function add

---
+++ functions/fisrt.js
@@ -0,0 +1,10 @@
+exports.handler = async () => {
+    console.log("function run");
+
+    const data = {name: "hasibul", age: 28, job: "work"}
+
+    return {
+        code: 200,
+        body: JSON.stringify(data)';'
+    }
+}
